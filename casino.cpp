10d7ee58243dae3131ce1ae5683f57d4e4cd396b
VietSit <trinhhoangviet0703@gmail.com>
2023-01-07 10:00:48 +0700

    update casino game

--- casino.cpp
+++ casino.cpp
@@ -38,14 +38,17 @@ void spin() {
     return ;
 }
 
-void check(int coin) {
+void updateCoin(int coin) {
     if (g_num[0] == g_num[1] && g_num[2] == g_num[1]) {
+        g_coinPlayer += coin * 49;
         cout << "Congratulation!!! You won " << coin*50 << " coin!!!" << endl;
     }
     else if (g_num[0] == g_num[1] || g_num[1] == g_num[2] || g_num[2] == g_num[0]) {
+        g_coinPlayer -= coin;
         cout << "So close!" << endl;
     }
     else {
+        g_coinPlayer -= coin;
         cout << "Unlucky!" << endl;
     }
     system("pause");
@@ -57,27 +60,57 @@ void home() {
     cout << "'-----------'" << endl;
     cout << "Your wallet: " << g_coinPlayer << " coins" << endl;
 }
-int main() {
-    srand(time(0));
-    home();
-    int coin, choice;
-loop:
-    cout << "Insert coin: ";
-    cin >> coin;
+
+void checkCoin(int& coin) {
     while (coin > g_coinPlayer) {
         cout << "Your wallet contains: " << g_coinPlayer << ", insert coin again: ";
         cin >> coin;
     }
+}
+
+void menu(int& choice, int& coin) {
+    cout << "Insert coin: ";
+    cin >> coin;
+    checkCoin(coin);
     cout << "1. Spin." << endl;
     cout << "2. Change coin." << endl;
     cout << "3. Exit." << endl;
     cout << "Select choice: ";
-selectChoice:
-    check(coin);
-    system("cls");
-    cout << ".-----------. O" << endl;
-    cout << "| 8 | 8 | 8 |_|" << endl;
-    cout << "'-----------'" << endl;
-    goto loop;
+    cin >> choice;
+}
+
+bool checkChoice(int& choice, int& coin) {
+    switch (choice) {
+        case 1:
+            system("cls");
+            spin();
+            return true;
+        case 2:
+            menu(choice, coin);
+            return checkChoice(choice, coin);
+        case 3:
+            return false;
+        default:
+            cout << "Wrong choice. Select choice again: ";
+            cin >> choice;
+            return checkChoice(choice,coin);
+    }
+}
+
+void abc() {
+    static int choice, coin;
+    menu(choice, coin);
+    if (checkChoice(choice,coin)) {
+        updateCoin(coin);
+        system("cls");
+        home();
+        abc();
+    }
+}
+
+int main() {
+    srand(time(0));
+    home();
+    abc();
     return 0;
 }
